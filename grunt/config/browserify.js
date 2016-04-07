5f6c571c3bfd78994bd285a03a665370de9dc63f
terw-dan <danny@yoast.com>
2016-04-07 13:30:38 +0200

    updates package.json

--- grunt/config/browserify.js
+++ grunt/config/browserify.js
@@ -1,7 +1,7 @@
 module.exports = {
 	build: {
 		files: {
-			"js/yoastseo-woo-plugin-311.js": [ "js/src/yoastseo-woo-plugin.js" ]
+			'js/yoastseo-woo-plugin-311.js': [ 'js/src/yoastseo-woo-plugin.js' ]
 		}
 	}
 };
