4bb52f05616e95b0ccbb534d69a44b6b0656fe9a
Andy Meerwaldt <andy@yoast.com>
2017-11-22 10:51:06 +0100

    Remove the link count columns

--- wpseo-woocommerce.php
+++ wpseo-woocommerce.php
@@ -480,8 +480,17 @@ class Yoast_WooCommerce_SEO {
 	 *
 	 * @return mixed
 	 */
-	function column_heading( $columns ) {
-		unset( $columns['wpseo-title'], $columns['wpseo-metadesc'], $columns['wpseo-focuskw'], $columns['wpseo-score'], $columns['wpseo-score-readability'] );
+	public function column_heading( $columns ) {
+		$keys_to_remove = array( 'wpseo-title', 'wpseo-metadesc', 'wpseo-focuskw', 'wpseo-score', 'wpseo-score-readability' );
+
+		if ( class_exists( 'WPSEO_Link_Columns' ) ) {
+			$keys_to_remove[] = 'wpseo-' . WPSEO_Link_Columns::COLUMN_LINKS;
+			$keys_to_remove[] = 'wpseo-' . WPSEO_Link_Columns::COLUMN_LINKED;
+		}
+
+		foreach( $keys_to_remove as $key_to_remove ) {
+			unset( $columns[ $key_to_remove ] );
+		}
 
 		return $columns;
 	}
