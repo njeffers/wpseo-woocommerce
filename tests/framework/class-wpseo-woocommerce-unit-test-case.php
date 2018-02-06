7f908f9e94ef0fc4685d9e030e3861a6a1060204
jrfnl <jrfnl@users.noreply.github.com>
2018-02-07 00:42:12 +0100

    Documentation: add some more missing descriptions

--- tests/framework/class-wpseo-woocommerce-unit-test-case.php
+++ tests/framework/class-wpseo-woocommerce-unit-test-case.php
@@ -11,8 +11,10 @@
 class WPSEO_WooCommerce_UnitTestCase extends WP_UnitTestCase {
 
 	/**
-	 * @param string $key
-	 * @param mixed  $value
+	 * Set up an HTTP post request.
+	 *
+	 * @param string $key   Array key.
+	 * @param mixed  $value Value.
 	 */
 	protected function set_post( $key, $value ) {
 		$_POST[ $key ]    = addslashes( $value );
@@ -20,7 +22,9 @@ class WPSEO_WooCommerce_UnitTestCase extends WP_UnitTestCase {
 	}
 
 	/**
-	 * @param string $key
+	 * Unset an HTTP post request.
+	 *
+	 * @param string $key Array key.
 	 */
 	protected function unset_post( $key ) {
 		unset( $_POST[ $key ], $_REQUEST[ $key ] );
@@ -34,7 +38,9 @@ class WPSEO_WooCommerce_UnitTestCase extends WP_UnitTestCase {
 	}
 
 	/**
-	 * @param string $string
+	 * Test expected output.
+	 *
+	 * @param string $string Expected output string.
 	 */
 	protected function expectOutput( $string ) {
 		$output = ob_get_contents();
