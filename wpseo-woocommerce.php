96458234f4a639306bef3a4252354901501900aa
Jimmy Comack <jimmy@yoast.com>
2018-07-24 10:35:54 +0200

    Bumped versions for release 7.9

--- wpseo-woocommerce.php
+++ wpseo-woocommerce.php
@@ -6,7 +6,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Yoast SEO: WooCommerce
- * Version:     7.8
+ * Version:     7.9
  * Plugin URI:  https://yoast.com/wordpress/plugins/yoast-woocommerce-seo/
  * Description: This extension to WooCommerce and Yoast SEO makes sure there's perfect communication between the two plugins.
  * Author:      Team Yoast
@@ -38,7 +38,7 @@ class Yoast_WooCommerce_SEO {
 	 *
 	 * @var string
 	 */
-	const VERSION = '7.8';
+	const VERSION = '7.9';
 
 	/**
 	 * Instance of the WooCommerce_SEO option management class.
