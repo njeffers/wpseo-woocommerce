59492eb51130306d06ca87a0174079fb6f219388
Jimmy Comack <jimmy@yoast.com>
2018-08-22 10:37:56 +0200

    Version bumps for RC2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ WooCommerce Yoast SEO
 =====================
 Requires at least: 4.8
 Tested up to: 4.9.8
-Stable tag: 8.0
+Stable tag: 8.1-RC2
 Requires PHP: 5.2.4
 Depends: Yoast SEO, WooCommerce
 
--- wpseo-woocommerce.php
+++ wpseo-woocommerce.php
@@ -6,7 +6,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Yoast SEO: WooCommerce
- * Version:     8.0
+ * Version:     8.1-RC2
  * Plugin URI:  https://yoast.com/wordpress/plugins/yoast-woocommerce-seo/
  * Description: This extension to WooCommerce and Yoast SEO makes sure there's perfect communication between the two plugins.
  * Author:      Team Yoast
@@ -38,7 +38,7 @@ class Yoast_WooCommerce_SEO {
 	 *
 	 * @var string
 	 */
-	const VERSION = '8.0';
+	const VERSION = '8.1-RC2';
 
 	/**
 	 * Instance of the WooCommerce_SEO option management class.
