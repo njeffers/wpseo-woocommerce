4ebd43004671a594548b44e1e1e9e9e871e01ea4
Anton Timmermans <email@atimmer.com>
2018-09-11 11:30:19 +0200

    Add case sensitivity plugin

--- tools/webpack/webpack.config.default.js
+++ tools/webpack/webpack.config.default.js
@@ -2,6 +2,7 @@ const _defaultsDeep = require( "lodash.defaultsDeep" );
 const path = require( "path" );
 const pkg = require( "../../package.json" );
 const UnminifiedWebpackPlugin = require( "unminified-webpack-plugin" );
+const CaseSensitivePathsPlugin = require( "case-sensitive-paths-webpack-plugin" );
 const { flattenVersionForFile } = require( "../version.js" );
 const webpack = require( "webpack" );
 
@@ -15,25 +16,26 @@ const defaultConfig = {
 	mode: "production",
 	devtool: "cheap-module-eval-source-map",
 	entry: {
-		'yoastseo-woo-plugin': path.join( __dirname, "../../", "js/src/yoastseo-woo-plugin.js" ),
-		'yoastseo-woo-replacevars': path.join( __dirname, "../../", "js/src/yoastseo-woo-replacevars.js" ),
-		'yoastseo-woo-worker': path.join( __dirname, "../../", "js/src/yoastseo-woo-worker.js" ),
+		"yoastseo-woo-plugin": path.join( __dirname, "../../", "js/src/yoastseo-woo-plugin.js" ),
+		"yoastseo-woo-replacevars": path.join( __dirname, "../../", "js/src/yoastseo-woo-replacevars.js" ),
+		"yoastseo-woo-worker": path.join( __dirname, "../../", "js/src/yoastseo-woo-worker.js" ),
 	},
 	output: {
 		path: path.join( __dirname, "../../", "js" ),
-		filename: "[name]-" + pluginVersionSlug + ".min.js"
+		filename: "[name]-" + pluginVersionSlug + ".min.js",
 	},
 	externals: externals,
 	optimization: {
-		minimize: true
+		minimize: true,
 	},
 	plugins: [
 		new webpack.DefinePlugin( {
 			"process.env": {
 				NODE_ENV: JSON.stringify( "production" ),
-			}
+			},
 		} ),
-		new UnminifiedWebpackPlugin()
+		new UnminifiedWebpackPlugin(),
+		new CaseSensitivePathsPlugin(),
 	],
 };
 
@@ -43,5 +45,5 @@ const defaults = ( config ) => {
 };
 
 module.exports = {
-	defaults
+	defaults,
 };
