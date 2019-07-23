d07afdb63056bfc78f3c5c1126ec050a15763653
Alexander Botteram <a.botteram@gmail.com>
2019-07-23 15:09:31 +0200

    Added brain monkey test

--- tests/classes/woocommerce-schema-test.php
+++ tests/classes/woocommerce-schema-test.php
@@ -3,6 +3,8 @@
 namespace Yoast\Tests\Classes;
 
 use \Yoast\Tests\TestCase;
+use \WPSEO_WooCommerce_Schema;
+use Brain\Monkey;
 
 /**
  * Class WooCommerce_Schema_Test.
@@ -14,8 +16,16 @@ class WooCommerce_Schema_Test extends TestCase {
 	/**
 	 * Tests a random test.
 	 */
-	public function test_random() {
-		$this->assertEquals( 2, ( 1 + 1 ) );
+	public function test_should_output_yoast_schema() {
+		Monkey\Filters\expectApplied( 'wpseo_json_ld_output' )->once()->andReturn( true );
+
+		$actual = WPSEO_WooCommerce_Schema::should_output_yoast_schema();
+		$this->assertEquals( true, $actual );
+
+		Monkey\Filters\expectApplied( 'wpseo_json_ld_output' )->once()->andReturn( false );
+
+		$actual = WPSEO_WooCommerce_Schema::should_output_yoast_schema();
+		$this->assertEquals( false, $actual );
 	}
 }
 
