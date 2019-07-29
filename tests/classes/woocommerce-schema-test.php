777e86c99cedc692742ce8e908dfe29513eb714c
Alexander Botteram <a.botteram@gmail.com>
2019-07-29 12:55:39 +0200

    Fixed namespace, add @covers to test and fixed doc comment

--- tests/classes/woocommerce-schema-test.php
+++ tests/classes/woocommerce-schema-test.php
@@ -1,20 +1,20 @@
 <?php
 
-namespace Yoast\Tests\Classes;
+namespace Yoast\WP\WooComerce\Tests\Classes;
 
-use \Yoast\Tests\TestCase;
+use Yoast\WP\Woocommerce\Tests\TestCase;
 use \WPSEO_WooCommerce_Schema;
 use Brain\Monkey;
 
 /**
  * Class WooCommerce_Schema_Test.
- *
- * @package Yoast\Tests\Classes
  */
 class WooCommerce_Schema_Test extends TestCase {
 
 	/**
-	 * Tests a random test.
+	 * Tests that should_output_yoast_schema returns the right value.
+	 *
+	 * @covers WPSEO_WooCommerce_Schema::should_output_yoast_schema
 	 */
 	public function test_should_output_yoast_schema() {
 		Monkey\Filters\expectApplied( 'wpseo_json_ld_output' )->once()->andReturn( true );
