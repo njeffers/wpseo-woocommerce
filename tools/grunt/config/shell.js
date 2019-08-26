d6d49f2318572eede6996290973fb18f58d36fbb
Andy Meerwaldt <andy@yoast.com>
2019-08-26 13:55:21 +0200

    Fix CS errors

--- tools/grunt/config/shell.js
+++ tools/grunt/config/shell.js
@@ -1,8 +1,8 @@
 // See https://github.com/sindresorhus/grunt-shell
-module.exports = function( grunt ) {
+module.exports = function() {
 	return {
-		"phpcs": {
+		phpcs: {
 			command: "php ./vendor/squizlabs/php_codesniffer/bin/phpcs",
-		}
+		},
 	};
 };
