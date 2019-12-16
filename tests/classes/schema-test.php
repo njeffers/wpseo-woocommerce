b7e29bb43bc7f03962b25a6df81be540749be511
IreneStr <irene@yoast.com>
2019-12-16 09:03:19 +0100

    Add parent::setUp

--- tests/classes/schema-test.php
+++ tests/classes/schema-test.php
@@ -18,6 +18,7 @@ class Schema_Test extends TestCase {
 	 * Test setup.
 	 */
 	public function setUp() {
+		parent::setUp();
 		// This constant is always defined by WooCommerce.
 		define( 'WC_VERSION', '3.8.1' ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
 	}
