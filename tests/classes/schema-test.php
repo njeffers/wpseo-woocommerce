a7e551aff6e7393f476e30a0d60f7104ec5ac1e0
Jesser Hulshof <jesserh16@gmail.com>
2020-04-02 10:40:02 +0200

    Updated test test_change_product_with_color
    It was newly added in trunk

--- tests/classes/schema-test.php
+++ tests/classes/schema-test.php
@@ -1177,6 +1177,8 @@ class Schema_Test extends TestCase {
 
 		$utils = Mockery::mock( 'alias:WPSEO_Utils' );
 		$utils->expects( 'get_home_url' )->once()->with()->andReturn( $canonical );
+		$utils->expects( 'format_json_encode' )->twice()->andReturn( 'TestProduct' );
+		Functions\expect( 'wp_strip_all_tags' )->twice()->andReturn( 'TestProduct' );
 
 		$product = Mockery::mock( 'WC_Product' );
 		$product->expects( 'get_id' )->times( 6 )->with()->andReturn( $product_id );
