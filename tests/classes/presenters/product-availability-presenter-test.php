508ea2d9bf382b230a6390910247abbc342ab991
Igor <igor@yoast.com>
2020-04-22 09:34:56 +0200

    Add product availability presenter tests

---
+++ tests/classes/presenters/product-availability-presenter-test.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Yoast\WP\Woocommerce\Tests\Classes\Presenters;
+
+use Mockery;
+use WPSEO_WooCommerce_Product_Availability_Presenter;
+use Yoast\WP\Woocommerce\Tests\TestCase;
+
+/**
+ * Class Product_Availability_Presenter_Test.
+ *
+ * @coversDefaultClass \WPSEO_WooCommerce_Product_Availability_Presenter
+ *
+ * @group presenters
+ */
+class Product_Availability_Presenter_Test extends TestCase {
+
+	/**
+	 * Holds the product.
+	 *
+	 * @var \WC_Product|\Mockery\MockInterface
+	 */
+	protected $product;
+
+	/**
+	 * Initializes the test setup.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		// Needs to exist as WPSEO_WooCommerce_Abstract_Product_Presenter depends on it.
+		Mockery::mock( 'overload:Yoast\WP\SEO\Presenters\Abstract_Indexable_Tag_Presenter' );
+
+		$this->product = Mockery::mock( 'WC_Product' );
+	}
+
+	/**
+	 * Tests the constructor.
+	 *
+	 * @covers ::__construct
+	 * @covers \WPSEO_WooCommerce_Abstract_Product_Availability_Presenter::__construct
+	 * @covers \WPSEO_WooCommerce_Abstract_Product_Presenter::__construct
+	 */
+	public function test_construct() {
+		$instance = new WPSEO_WooCommerce_Product_Availability_Presenter( $this->product, false, true );
+
+		$this->assertAttributeEquals( $this->product, 'product', $instance );
+		$this->assertAttributeEquals( false, 'is_on_backorder', $instance );
+		$this->assertAttributeEquals( true, 'is_in_stock', $instance );
+	}
+
+	/**
+	 * Tests the tag format.
+	 */
+	public function test_tag_format() {
+		$instance = new WPSEO_WooCommerce_Product_Availability_Presenter( $this->product, false );
+
+		$this->assertAttributeEquals( '<meta property="product:availability" content="%s" />', 'tag_format', $instance );
+	}
+
+	/**
+	 * Tests that the fallback is out of stock.
+	 *
+	 * @covers ::get
+	 */
+	public function test_get() {
+		$instance = new WPSEO_WooCommerce_Product_Availability_Presenter( $this->product, false, false );
+
+		$this->assertEquals( 'out of stock', $instance->get() );
+	}
+
+	/**
+	 * Tests on backorder.
+	 *
+	 * @covers ::get
+	 */
+	public function test_get_on_backorder() {
+		$instance = new WPSEO_WooCommerce_Product_Availability_Presenter( $this->product, true );
+
+		$this->assertEquals( 'available for order', $instance->get() );
+	}
+
+	/**
+	 * Tests in stock.
+	 *
+	 * @covers ::get
+	 */
+	public function test_get_in_stock() {
+		$instance = new WPSEO_WooCommerce_Product_Availability_Presenter( $this->product, false, true );
+
+		$this->assertEquals( 'instock', $instance->get() );
+	}
+}
