69c7962aa36b577f7ce8b11144e01154d12f58a7
Igor <igor@yoast.com>
2020-04-22 10:42:51 +0200

    Add product price currency presenter tests

---
+++ tests/classes/presenters/product-price-currency-presenter-test.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Yoast\WP\Woocommerce\Tests\Classes\Presenters;
+
+use Mockery;
+use WPSEO_WooCommerce_Product_Price_Currency_Presenter;
+use Yoast\WP\Woocommerce\Tests\TestCase;
+use function Brain\Monkey\Functions\expect;
+
+/**
+ * Class Product_Price_Currency_Presenter_Test.
+ *
+ * @coversDefaultClass \WPSEO_WooCommerce_Product_Price_Currency_Presenter
+ *
+ * @group presenters
+ */
+class Product_Price_Currency_Presenter_Test extends TestCase {
+
+	/**
+	 * Holds the product.
+	 *
+	 * @var \WC_Product|\Mockery\MockInterface
+	 */
+	protected $product;
+
+	/**
+	 * Holds the instance to test.
+	 *
+	 * @var WPSEO_WooCommerce_Product_Price_Currency_Presenter
+	 */
+	protected $instance;
+
+	/**
+	 * Initializes the test setup.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		// Needs to exist as WPSEO_WooCommerce_Abstract_Product_Presenter depends on it.
+		Mockery::mock( 'overload:Yoast\WP\SEO\Presenters\Abstract_Indexable_Tag_Presenter' );
+
+		$this->product = Mockery::mock( 'WC_Product' );
+
+		$this->instance = new WPSEO_WooCommerce_Product_Price_Currency_Presenter( $this->product );
+	}
+
+	/**
+	 * Tests the constructor.
+	 *
+	 * @covers ::__construct
+	 */
+	public function test_construct() {
+		$this->assertAttributeEquals( $this->product, 'product', $this->instance );
+	}
+
+	/**
+	 * Tests the tag format.
+	 */
+	public function test_tag_format() {
+		$this->assertAttributeEquals( '<meta property="product:price:currency" content="%s" />', 'tag_format', $this->instance );
+	}
+
+	/**
+	 * Tests that the currency is retrieved.
+	 *
+	 * @covers ::get
+	 */
+	public function test_get() {
+		expect( 'get_woocommerce_currency' )->once()->andReturn( 'EUR' );
+
+		$this->assertSame( 'EUR', $this->instance->get() );
+	}
+}
