f8de09a8a58df6f6e4567b7c06df50ff603b8bbf
Hans-Christiaan <hans-christiaan@yoast.com>
2020-05-01 11:45:37 +0200

    Added first tests for Twitter product gallery image fallback.

--- classes/woocommerce-twitter.php
+++ classes/woocommerce-twitter.php
@@ -26,24 +26,22 @@ class WPSEO_WooCommerce_Twitter {
 	 * @return string The image fallback.
 	 */
 	public function fallback_to_product_gallery_image( $twitter_image, $presentation ) {
-		// We should only fall back to the Twitter image if OpenGraph is disabled.
-		if ( ! $presentation->context->open_graph_enabled ) {
+		// We should only fall back to the Twitter image if OpenGraph is disabled and no other Twitter image is set.
+		if ( ! $presentation->context->open_graph_enabled && ! $twitter_image ) {
 			$object = $presentation->model;
 
-			// If a twitter image is set, do not overwrite it.
-			if ( $object->twitter_image ) {
-				return $twitter_image;
-			}
-
 			// Fall back to the first image in the product gallery.
-			if ( $object->object_sub_type === 'product' ) {
+			if ( $object->object_type === 'post' && $object->object_sub_type === 'product' ) {
 				$product = \wc_get_product( $object->object_id );
 
 				if ( $product ) {
 					$gallery_image_ids      = $product->get_gallery_image_ids();
 					$first_gallery_image_id = \reset( $gallery_image_ids );
 
-					return YoastSEO()->helpers->twitter->image->get_by_id( $first_gallery_image_id );
+					if ( $first_gallery_image_id ) {
+						return YoastSEO()->helpers->twitter->image->get_by_id( $first_gallery_image_id );
+					}
+					return '';
 				}
 			}
 		}
---
+++ tests/classes/twitter-test.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Yoast\WP\Woocommerce\Tests\Classes;
+
+use Mockery;
+use Yoast\WP\SEO\Presentations\Indexable_Presentation;
+use Yoast\WP\Woocommerce\Tests\TestCase;
+
+use Brain\Monkey;
+
+/**
+ * Class OpenGraph_Test
+ *
+ * @coversDefaultClass \WPSEO_WooCommerce_Twitter
+ */
+class OpenGraph_Test extends TestCase {
+
+	/**
+	 * The Twitter class under test.
+	 *
+	 * @var \WPSEO_WooCommerce_Twitter
+	 */
+	private $instance;
+
+	public function setUp() {
+		$this->instance = new \WPSEO_WooCommerce_Twitter();
+
+		parent::setUp();
+	}
+
+	/**
+	 * Tests that the right hooks are registered.
+	 *
+	 * @covers ::register_hooks
+	 */
+	public function test_register_hooks() {
+		Monkey\Filters\expectAdded( 'wpseo_twitter_image' );
+		$this->instance->register_hooks();
+	}
+
+	/**
+	 * Tests that the twitter image correctly falls back to the first
+	 * product gallery image.
+	 *
+	 * @covers ::fallback_to_product_gallery_image
+	 */
+	public function test_fallback_to_product_gallery_image() {
+		// Empty image, so should provide a fallback.
+		$empty_image_url = '';
+		$fallback_image_url = 'http://basic.wordpress.test/wp-content/uploads/2020/04/teddy_2.jpg';
+		$context         = (object) [
+			'open_graph_enabled' => false,
+		];
+		$model           = (object) [
+			'object_id'       => 13,
+			'object_type'     => 'post',
+			'object_sub_type' => 'product',
+		];
+		$product = Mockery::mock( 'WC_Product' )->makePartial();
+		$product->expects( 'get_gallery_image_ids' )
+			->andReturn( [ 21, 23, 24 ] );
+
+		$presentation = $this->mock_presentation( $context, $model );
+
+		Monkey\Functions\expect( 'wc_get_product' )
+			->with( $model->object_id )
+			->andReturn( $product );
+
+		$this->mock_yoastseo( 21, $fallback_image_url );
+
+		$image_url = $this->instance->fallback_to_product_gallery_image( $empty_image_url, $presentation );
+
+		$this->assertSame( $fallback_image_url, $image_url );
+	}
+
+	/**
+	 * Mocks the Indexable presentation.
+	 *
+	 * @param object $context The meta context.
+	 * @param object $model   The model.
+	 *
+	 * @return Mockery\MockInterface The mock presentation
+	 */
+	private function mock_presentation( $context, $model ) {
+		$presentation = \Mockery::mock();
+
+		$presentation->context = $context;
+		$presentation->model   = $model;
+
+		return $presentation;
+	}
+
+	/**
+	 * Mocks the YoastSEO function for the Twitter image tests.
+	 *
+	 * @param int    $fallback_image_id  The image ID of the fallback image.
+	 * @param string $fallback_image_url The image URL of the fallback image.
+	 */
+	private function mock_yoastseo( $fallback_image_id, $fallback_image_url ) {
+		$twitter_image_helper = Mockery::mock()->makePartial();
+		$twitter_image_helper
+			->expects( 'get_by_id' )
+			->with( $fallback_image_id )
+			->andReturn( $fallback_image_url  );
+
+		$surfaces = (object) [
+			'helpers' => (object) [
+				'twitter' => (object) [
+					'image' => $twitter_image_helper,
+				],
+			],
+		];
+
+		Monkey\Functions\expect( 'YoastSEO' )
+			->andReturn( $surfaces );
+	}
+}
